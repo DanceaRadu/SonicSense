df49de2dddd13ed56bdbac6bb3aea9ae3f194976
DanceaRadu <radu.dancea@gmail.com>
2025-04-08 16:50:25 +0100

    Added loading user settings from json file. Added settings button. Small refactoring.

--- src/main.py
+++ src/main.py
@@ -9,18 +9,41 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
 from matplotlib import cm
+import json
 
 class PiCamApp:
     def __init__(self, root):
         self.root = root
+        self.settings_path = "user_settings.json"
+        self.set_configuration_from_user_settings()
+
         self.root.title("SonicSense")
+        self.root.attributes('-fullscreen', True)
+        # self.root.overrideredirect(True)
+        self.root.geometry(f"{self.root.winfo_screenwidth()}x{self.root.winfo_screenheight()}+0+0")
+        self.root.update_idletasks()
+
         HelperService.ensure_v4l2loopback_device_exists()
 
         # Create GUI components
         self.video_label = tk.Label(root)
-        self.video_label.pack()
+        self.video_label.pack(fill=tk.BOTH, expand=True)
 
-        self.set_configuration_from_user_settings()
+        gear_img = Image.open("resources/icons/settings_icon.png")
+        gear_img = gear_img.resize((40, 40), Image.Resampling.LANCZOS)
+        self.gear_photo = ImageTk.PhotoImage(gear_img)
+
+        self.settings_button = tk.Button(
+            self.root,
+            image=self.gear_photo,
+            command=self.open_settings_window,
+            bd=0,
+            bg="#000",
+            activebackground="#000",
+            relief=tk.FLAT,
+            cursor="hand2"
+        )    
+        self.settings_button.place(relx=0.98, rely=0.95, anchor="se")
 
         # Start the libcamera-vid + ffmpeg pipeline
         self.pipeline_cmd = (
@@ -40,8 +63,8 @@ class PiCamApp:
         # Give the camera a second to warm up
         time.sleep(2)
 
-        self.cap = cv2.VideoCapture("/dev/video10", cv2.CAP_V4L2)
-        if not self.cap.isOpened():
+        self.video_capture = cv2.VideoCapture("/dev/video10", cv2.CAP_V4L2)
+        if not self.video_capture.isOpened():
             print("❌ Could not open /dev/video10")
             self.cleanup()
             exit()
@@ -52,16 +75,47 @@ class PiCamApp:
 
         self.update_frame()
         self.root.bind('<Alt-F4>', self.on_close)
+        self.root.bind("<Escape>", self.on_close)
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
     def set_configuration_from_user_settings(self):
-        self.sound_threshold = 1.0
-        self.frequency = 1000
-        self.bandwidth = 1
+        default_config = {
+            "sound_threshold": 1.0,
+            "frequency": 1000,
+            "bandwidth": 1
+        }
 
+        try:
+            if os.path.exists(self.settings_path):
+                with open(self.settings_path, "r") as f:
+                    user_config = json.load(f)
+            else:
+                user_config = {}
+
+        except (json.JSONDecodeError, IOError) as e:
+            print(f"Error loading user settings: {e}")
+            user_config = {}
+
+        self.sound_threshold = user_config.get("sound_threshold", default_config["sound_threshold"])
+        self.frequency = user_config.get("frequency", default_config["frequency"])
+        self.bandwidth = user_config.get("bandwidth", default_config["bandwidth"])
+
+        merged_config = {
+            "sound_threshold": self.sound_threshold,
+            "frequency": self.frequency,
+            "bandwidth": self.bandwidth
+        }
+        self.set_user_settings(merged_config)
+
+    def set_user_settings(self, user_settings):
+        try:
+            with open(self.settings_path, "w") as f:
+                json.dump(user_settings, f, indent=4)
+        except IOError as e:
+            print(f"Failed to save user settings: {e}")
 
     def update_frame(self):
-        ret, frame = self.cap.read()
+        ret, frame = self.video_capture.read()
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -88,12 +142,26 @@ class PiCamApp:
 
                 frame = cv2.addWeighted(frame, 0.7, bf_color, 0.3, 0)
 
-            image = Image.fromarray(frame)
+            window_width = self.root.winfo_width()
+            if window_width <= 1:
+                window_width = 800 
+
+            orig_height, orig_width, _ = frame.shape
+
+            aspect_ratio = orig_height / orig_width
+            new_width = window_width
+            new_height = int(window_width * aspect_ratio)
+
+            resized_frame = cv2.resize(frame, (new_width-2, new_height-2))
+            image = Image.fromarray(resized_frame)
             photo = ImageTk.PhotoImage(image=image)
             self.video_label.imgtk = photo
             self.video_label.configure(image=photo)
 
         self.root.after(30, self.update_frame)
+
+    def open_settings_window(self):
+        print("Opening settings window...")
 
     def on_close(self):
         self.cleanup()
@@ -101,8 +169,8 @@ class PiCamApp:
 
     def cleanup(self):
         print("Cleaning up...")
-        if self.cap:
-            self.cap.release()
+        if self.video_capture:
+            self.video_capture.release()
         os.killpg(os.getpgid(self.pipeline_process.pid), signal.SIGTERM)
 
 if __name__ == "__main__":
